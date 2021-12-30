9529738362359bef165953c067da99134d4d21b2
Manas Mahale <36999652+Manas02@users.noreply.github.com>
2021-12-30 15:06:28 +0530

    Update AntiGPT.py

--- AntiGPT.py
+++ AntiGPT.py
@@ -59,7 +59,7 @@ mconf = GPTConfig(train_dataset.vocab_size, train_dataset.block_size,
 model = GPT(mconf)
 
 
-tconf = TrainerConfig(max_epochs=100, batch_size=64, learning_rate=1e-2,
+tconf = TrainerConfig(max_epochs=100, batch_size=64, learning_rate=3e-4,
                       lr_decay=True, warmup_tokens=512*20, final_tokens=2*len(train_dataset)*block_size,
                       ckpt_path='./ckpt/model.bin')
 trainer = Trainer(model, train_dataset, None, tconf)
